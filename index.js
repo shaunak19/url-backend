e4cf592a7c896be6ed4dd90441514ca1affde25f
Shaunak Das <51281688+shaunak19@users.noreply.github.com>
2021-10-25 13:05:54 +0530

    port updated 2

--- index.js
+++ index.js
@@ -80,6 +80,6 @@ app.post("/user/login", async function(req,res){
     }
 })
 
-app.listen( process.env.port || 3000,function(){
+app.listen( process.env || 3000,function(){
     console.log(`Server is running in PORT ${process.env.port}`);
 })
